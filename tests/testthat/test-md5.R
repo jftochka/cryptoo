581089a6829465a453e45adac2a40a0757ee1cd5
JorgeRiescoDavila <jorgeriesco0@gmail.com>
2022-01-08 19:24:09 +0100

    add md5 test
    add test for long stringadd md5 test
    
    add test for long stringadd md5 test
    
    add test for long stringadd md5 test
    
    add test for long stringadd md5 test
    
    add test for long string

--- tests/testthat/test-md5.R
+++ tests/testthat/test-md5.R
@@ -1,3 +1,10 @@
-test_that("multiplication works", {
-  expect_equal(md5(utf8ToInt("")),"d41d8cd98f00b204e9800998ecf8427e")
+test_that("empty string hash", {
+  expect_equal(md5(utf8ToInt("")),
+               "d41d8cd98f00b204e9800998ecf8427e")
 })
+
+test_that("long(more than 1 block) string hash", {
+  expect_equal(md5(utf8ToInt("The quick brown fox jumps over the lazy dog")),
+               "e107d9d372bb6826bd81d3542a419d6")
+})
+
